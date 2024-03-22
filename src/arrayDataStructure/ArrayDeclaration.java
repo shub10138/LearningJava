0afe8068ecab7a722164da042460122522f04bb1
shubh <shubh@Shubham-Laptop.lan>
2024-03-23 03:20:29 +0530

    Arrays

---
+++ src/arrayDataStructure/ArrayDeclaration.java
@@ -0,0 +1,18 @@
+package arrayDataStructure;
+
+public class ArrayDeclaration {
+
+	public static void main(String[] args) {
+		int[] a = new int[4];
+
+//		int a[], b;
+//		a = new int[4];
+//		b = 10;
+
+//		int[] a, b;
+//		a = new int[4];
+//		b = 10;
+
+	}
+
+}
