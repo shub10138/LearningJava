899af21c1b0e63fb8ff05c631afb11ff46053370
shubh <shubh@Shubham-Laptop.lan>
2024-07-15 04:28:24 +0530

    Comparator

--- src/LambdaExpressions/SortCoordinates.java
+++ src/LambdaExpressions/SortCoordinates.java
@@ -6,7 +6,8 @@ public class SortCoordinates {
 
 	public static void main(String[] args) {
 		Points[] p = { new Points(10, 20), new Points(5, 30), new Points(20, 50), new Points(2, 40) };
-
+		// here p1.x - p2.x returns negative(p1.x is smaller), zero(p1.x=p2.x) and
+		// positive(p2.x is smaller) value
 		Arrays.sort(p, (p1, p2) -> p1.x - p2.x);
 		for (Points pt : p) {
 			System.out.println(pt.x + "," + pt.y);
